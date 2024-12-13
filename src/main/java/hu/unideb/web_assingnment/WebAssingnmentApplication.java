b6e16e8301f286cbe289d99afcf8b15e1d7029b2
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-12-13 22:54:56 +0100

    Configure project

---
+++ src/main/java/hu/unideb/web_assingnment/WebAssingnmentApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.web_assingnment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAssingnmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAssingnmentApplication.class, args);
+	}
+
+}
