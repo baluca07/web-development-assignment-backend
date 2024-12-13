8d31ea841ad388ebfd09e5738536ffc2145ce0cb
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-12-14 00:42:09 +0100

    Fixed EmployeeRepository

--- src/main/java/hu/unideb/web_assingnment/data/repositories/EmployeeRepository.java
+++ src/main/java/hu/unideb/web_assingnment/data/repositories/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package hu.unideb.web_assingnment.data.repositories;
 
+import hu.unideb.web_assingnment.data.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeRepository, Long> {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+
 }
