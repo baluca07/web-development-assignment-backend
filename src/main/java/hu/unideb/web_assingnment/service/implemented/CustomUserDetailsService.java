e85c330b48d0ee4127e82a0584a1658f95e8ecdd
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-12-20 15:14:26 +0100

    Added CustomUserDetailsService

---
+++ src/main/java/hu/unideb/web_assingnment/service/implemented/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package hu.unideb.web_assingnment.service.implemented;
+
+import hu.unideb.web_assingnment.data.entities.UserEntity;
+import hu.unideb.web_assingnment.data.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole().name()))
+        );
+    }
+}
+
